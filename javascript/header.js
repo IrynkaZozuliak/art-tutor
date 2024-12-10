96e87d1b98e938c79253ca5a34b7b471d746297b
IrynkaZozuliak <108537570+IrynkaZozuliak@users.noreply.github.com>
2024-12-10 18:30:11 +0000

    sdfghj

---
+++ javascript/header.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector(".header__nav-bar");
+const menuLinks = document.querySelectorAll(".header__menu-link");
+
+menuLinks.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    navBar.classList.remove("active");
+  });
+});
+
+hamburger.addEventListener("click", function () {
+  navBar.classList.toggle("active");
+});
