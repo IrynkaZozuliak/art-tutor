b38622344e855a3c46bb5a68b9f7407477ba2265
IrynkaZozuliak <108537570+IrynkaZozuliak@users.noreply.github.com>
2025-07-18 17:08:08 +0000

    fghjk

--- javascript/contact-popup.js
+++ javascript/contact-popup.js
@@ -1,1 +1,18 @@
-document.getElementById("open-popup-btn").addEventListener("click",function(){})
+ 
+
+document.getElementById("dismiss-popup-btn").addEventListener("click", function () {
+  document.getElementsByClassName("popup")[0].classList.remove("active");
+
+  // Показати кнопку "Contact Me" знову
+  document.getElementById("contact-popup-btn").style.display = "inline-block";
+});
+
+document.getElementById("contact-popup-btn").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  // Показати popup
+  document.getElementsByClassName("popup")[0].classList.add("active");
+
+  // Сховати кнопку
+  document.getElementById("contact-popup-btn").style.display = "none";
+});
